b945ad101db52ae769a174d8741cf4efe283fdc5
case <56609440+dcase808@users.noreply.github.com>
2022-02-06 00:42:01 +0100

    Update db_connect.py

--- db_connect.py
+++ db_connect.py
@@ -1,6 +1,6 @@
 import pymongo
 
-client = pymongo.MongoClient("xxx")
+client = pymongo.MongoClient("mongodb+srv://admin:BrDtRoW7ZNDT3Wyn@headlines-db.a3a1p.mongodb.net/tvp-vs-tvn?retryWrites=true&w=majority")
 db = client.tvp_vs_tvn
 tvp = db.tvp
 tvn = db.tvn
